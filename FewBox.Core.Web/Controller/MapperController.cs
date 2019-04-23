da4e36708741f0c4261e3509a75ac7b3d967b1e0
Peter Pang <mv@live.cn>
2019-04-23 15:26:34 +0800

    Add mapper controller.

---
+++ FewBox.Core.Web/Controller/MapperController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+
+namespace FewBox.Core.Web.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    abstract class MapperController : ControllerBase
+    {
+        private IMapper Mapper { get; set; }
+
+        [FromHeader(Name = "Authorization")]
+        public string Authorization { get; set; }
+
+        protected MapperController(IMapper mapper)
+        {
+            this.Mapper = mapper;
+        }
+    }
+}
